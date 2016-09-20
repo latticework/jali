7c18c12cdf0a03c4aaf4c88a8a45dab135751a7c
Kenneth Brubaker <kennethbrubaker@outlook.com>
2016-09-20 08:27:56 -0500

    Improved package build

--- packages/@jali/util/webpackfile.js
+++ packages/@jali/util/webpackfile.js
@@ -17,7 +17,7 @@ const scopeDir = `${packagesDir}/${npmScope}`;
 const packageDir = `${scopeDir}/${packageName}`;
 
 const distDir = `./${distDirName}`;
-const distPackagesDir = `${distDir}/${packagesDirName}`;
+const distPackagesDir = `${distDir}/${packagesDistDirName}`;
 const distScopeDir = `${distPackagesDir}/${npmScope}`;
 const distPackageDir = `${distScopeDir}/${packageName}`;
 const distPackageEntryPoint = `${distPackageDir}/${entryPointName}`;
@@ -42,6 +42,7 @@ module.exports = function(options) {
             'transform-class-properties',
             'transform-decorators-legacy',
             'transform-es2015-function-name',
+            'transform-es2015-modules-commonjs',
             'transform-function-sent',
             'transform-exponentiation-operator',
             [
@@ -68,13 +69,24 @@ module.exports = function(options) {
       new CopyWebpackPlugin([
         {
           context: packageDir,
+          from: '.npmignore',
+          to: '..',
+        },
+        {
+          context: packageDir,
           from: 'package.json',
-          to: distPackageDir,
+          to: '..',
         },
         {
           context: packageDir,
           from: 'README.md',
-          to: distPackageDir,
+          to: '..',
+        },
+        {
+          context: packageDir,
+          from: 'LICENCE',
+          to: '..',
+          toType: 'file',
         },
       ]),
     ],
