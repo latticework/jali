0947c2d5951cab37f2d0db9c35864e2639170f89
Kenneth Brubaker <kennethbrubaker@outlook.com>
2017-01-30 19:47:05 -0600

    Addressed semaphoreci status check failure
    See: https://semaphoreci.com/latticework/jali/branches/clavecoder-0105-fixbithoundissues/builds/4

--- packages/@jali-ms/util/testing/test-description.ts
+++ packages/@jali-ms/util/testing/test-description.ts
@@ -3,9 +3,9 @@ import { default as RepoPackage } from './repo-package';
 import { default as TestDisposition} from './test-disposition';
 import { default as TestType } from './test-type';
 
+/* tslint:disable */
 // TODO: Although linting seems to work locally, it fails on bitHound
 // See: https://www.bithound.io/github/latticework/jali/blob/a3aea32e00015f851838240cfb57e1755140c7f7/packages/@jali-ms/util/testing/test-description.ts
-/* tslint:disable */
 
 interface TestDescription {
   readonly type: TestType;
